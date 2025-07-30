85072f8c2ed2530f17bd93ba11f2a37b56c04d38
SysAlien <arvinjnr@gmail.com>
2025-07-30 07:57:28 +0100

    Create Patched_BfsInsertPolicyEntry.cpp

---
+++ Patched_BfsInsertPolicyEntry.cpp
@@ -0,0 +1,284 @@
+
+/* WARNING: Globals starting with '_' overlap smaller symbols at the same address */
+
+void BfsInsertPolicyEntry
+               (dword *param_1,undefined8 param_2,longlong param_3,undefined8 param_4,
+               longlong param_5,longlong param_6,longlong *param_7)
+
+{
+  longlong *plVar1;
+  undefined8 *puVar2;
+  longlong *plVar3;
+  code *pcVar4;
+  bool bVar5;
+  bool bVar6;
+  uint uVar7;
+  longlong lVar8;
+  longlong lVar9;
+  longlong lVar10;
+  longlong lVar11;
+  ulonglong uVar12;
+  undefined8 uVar13;
+  dword *pdVar14;
+  undefined1 auStackY_118 [32];
+  undefined8 local_e0;
+  char local_d8;
+  longlong local_d0;
+  undefined8 local_c8;
+  longlong local_c0;
+  dword *local_b8;
+  undefined8 local_b0;
+  dword local_a8 [4];
+  undefined8 local_98;
+  undefined8 uStack_90;
+  longlong *local_88;
+  undefined8 *local_60;
+  undefined8 local_58;
+  ulonglong local_50;
+  
+  local_50 = __security_cookie ^ (ulonglong)auStackY_118;
+  lVar10 = 0;
+  local_d0 = param_5;
+  local_88 = param_7;
+  bVar5 = false;
+  local_98 = 0;
+  uStack_90 = 0;
+  local_b0 = 0;
+  local_a8[0] = 0;
+  local_a8[1] = 0;
+  local_a8[2] = 0;
+  local_a8[3] = 0;
+  local_c0 = 0;
+  local_d8 = '\0';
+  bVar6 = false;
+  local_e0 = param_2;
+  local_c8 = param_4;
+  local_b8 = param_1;
+  KeEnterCriticalRegion();
+  ExAcquirePushLockExclusiveEx(param_3,0);
+  uVar12 = *(ulonglong *)(param_3 + 8);
+  lVar8 = BfsLookupPolicyEntryHashTable(uVar12,local_c8,param_5,param_6);
+  lVar11 = local_d0;
+  if (lVar8 == 0) {
+    lVar9 = ExAllocatePool2(0x100,(ulonglong)*(byte *)(local_d0 + 1) * 4 + 8,0x53736642);
+    if ((lVar9 == 0) ||
+       (lVar10 = ExAllocatePool2(0x100,(ulonglong)*(byte *)(param_6 + 1) * 4 + 8,0x53736642),
+       lVar10 == 0)) goto LAB_000068ef;
+    uVar12 = (ulonglong)((uint)*(byte *)(lVar11 + 1) * 4 + 8);
+    uVar7 = RtlCopySid(uVar12,lVar9,lVar11);
+    if ((int)uVar7 < 0) {
+LAB_00006981:
+      if (3 < DAT_00016000) {
+        local_e0 = CONCAT44(local_e0._4_4_,uVar7);
+LAB_00006907:
+        local_60 = &local_e0;
+        local_58 = 4;
+        _tlgWriteTransfer_EtwWriteTransfer(uVar12,&DAT_00013c91);
+      }
+    }
+    else {
+      uVar12 = (ulonglong)((uint)*(byte *)(param_6 + 1) * 4 + 8);
+      uVar7 = RtlCopySid(uVar12,lVar10,param_6);
+      if ((int)uVar7 < 0) goto LAB_00006981;
+      lVar8 = ExAllocatePool2(0x100,0x98,0x45736642);
+      if (lVar8 != 0) {
+        LOCK();
+        *(int *)(lVar8 + 0x90) = *(int *)(lVar8 + 0x90) + 1;
+        UNLOCK();
+        lVar11 = ExAllocatePool2(0x40,0x18,0x76736642);
+        *(longlong *)(lVar8 + 0x28) = lVar11;
+        if (lVar11 != 0) {
+          *(longlong *)(lVar8 + 0x18) = lVar9;
+          uVar12 = Feature_3148938554__private_IsEnabledDeviceUsageNoInline();
+          *(longlong *)(lVar8 + 0x20) = lVar10;
+          if ((int)uVar12 != 0) {
+            lVar9 = 0;
+          }
+          uVar12 = Feature_3148938554__private_IsEnabledDeviceUsageNoInline();
+          *(undefined4 *)(lVar8 + 0x38) = 0x10000001;
+          *(undefined4 *)(lVar8 + 0x68) = 0;
+          *(undefined8 *)(lVar8 + 0x70) = 0;
+          *(undefined8 *)(lVar8 + 0x78) = 0;
+          *(undefined2 *)(lVar8 + 0x72) = 0;
+          if ((int)uVar12 != 0) {
+            lVar10 = 0;
+          }
+          *(undefined8 *)(lVar8 + 0x78) = 0;
+          *(undefined8 *)(lVar8 + 0x80) = 0;
+          *(undefined8 *)(lVar8 + 0x88) = 0;
+          *(undefined2 *)(lVar8 + 0x82) = 0;
+          *(undefined8 *)(lVar8 + 0x88) = 0;
+          KeInitializeEvent(*(undefined8 *)(lVar8 + 0x28),0,0);
+          uVar12 = *(ulonglong *)(param_3 + 8);
+          uVar7 = BfsInsertEntryHashTable(uVar12,local_c8,lVar8);
+          if (-1 < (int)uVar7) {
+            LOCK();
+            *(int *)(lVar8 + 0x90) = *(int *)(lVar8 + 0x90) + 1;
+            UNLOCK();
+            plVar1 = (longlong *)(param_3 + 0x10);
+            local_d8 = '\x01';
+            if ((longlong *)*plVar1 == plVar1) {
+              ExSetTimer(*(undefined8 *)(param_3 + 0x20),0xffffffffee1e5d00,300000000);
+            }
+            puVar2 = *(undefined8 **)(param_3 + 0x18);
+            plVar3 = (longlong *)(lVar8 + 0x40);
+            if ((longlong *)*puVar2 != plVar1) goto LAB_00006de1;
+            *plVar3 = (longlong)plVar1;
+            *(undefined8 **)(lVar8 + 0x48) = puVar2;
+            *puVar2 = plVar3;
+            *(longlong **)(param_3 + 0x18) = plVar3;
+            LOCK();
+            *(undefined8 *)(lVar8 + 0x60) = _DAT_fffff78000000014;
+            UNLOCK();
+            goto LAB_00006af0;
+          }
+          goto LAB_00006981;
+        }
+      }
+LAB_000068ef:
+      uVar12 = 0xc0000017;
+      uVar7 = 0xc0000017;
+      if (3 < DAT_00016000) {
+        local_e0 = CONCAT44(local_e0._4_4_,0xc0000017);
+        uVar7 = 0xc0000017;
+        goto LAB_00006907;
+      }
+    }
+LAB_00006c44:
+    ExReleasePushLockExclusiveEx(param_3);
+    KeLeaveCriticalRegion();
+    bVar5 = false;
+    bVar6 = false;
+    if ((int)uVar7 < 0) {
+LAB_00006c71:
+      if (lVar8 != 0) {
+LAB_00006c76:
+        BfsDereferencePolicyEntryEx(lVar8,'\0');
+      }
+      if (local_d8 != '\0') {
+        KeEnterCriticalRegion();
+        ExAcquirePushLockExclusiveEx(param_3,0);
+        lVar11 = BfsLookupPolicyEntryHashTable
+                           (*(undefined8 *)(param_3 + 8),local_c8,local_d0,param_6);
+        if (lVar11 == 0) {
+          ExReleasePushLockExclusiveEx(param_3,0);
+          KeLeaveCriticalRegion();
+        }
+        else {
+          plVar1 = (longlong *)(lVar11 + 0x40);
+          *(undefined4 *)(lVar11 + 0x38) = 1;
+          lVar8 = *plVar1;
+          if ((*(longlong **)(lVar8 + 8) != plVar1) ||
+             (plVar3 = *(longlong **)(lVar11 + 0x48), (longlong *)*plVar3 != plVar1)) {
+LAB_00006de1:
+            pcVar4 = (code *)swi(0x29);
+            (*pcVar4)(3);
+            pcVar4 = (code *)swi(3);
+            (*pcVar4)();
+            return;
+          }
+          *plVar3 = lVar8;
+          *(longlong **)(lVar8 + 8) = plVar3;
+          uVar12 = Feature_Servicing_BfsGAFeature__private_IsEnabledDeviceUsageNoInline();
+          if ((int)uVar12 != 0) {
+            *plVar1 = 0;
+            *(undefined8 *)(lVar11 + 0x48) = 0;
+          }
+          ExReleasePushLockExclusiveEx(param_3);
+          KeLeaveCriticalRegion();
+          KeSetEvent(*(undefined8 *)(lVar11 + 0x28),0,0);
+          BfsDereferencePolicyEntryEx(lVar11,'\0');
+        }
+      }
+      if (bVar5) {
+        RtlFreeUnicodeString(local_a8);
+      }
+      if (bVar6) {
+        RtlFreeUnicodeString(&local_98);
+      }
+      if (lVar9 != 0) {
+        ExFreePoolWithTag(lVar9,0);
+      }
+      if (lVar10 != 0) {
+        ExFreePoolWithTag(lVar10,0);
+      }
+    }
+    if (local_c0 != 0) {
+      FltClose();
+    }
+  }
+  else {
+    lVar9 = lVar10;
+    if ((*(uint *)(lVar8 + 0x38) >> 0x1c & 1) == 0) {
+      if (*(uint *)(lVar8 + 0x38) != 2) {
+        uVar7 = 0xc0000001;
+        goto LAB_00006981;
+      }
+      *(undefined4 *)(lVar8 + 0x38) = 0x10000001;
+      KeResetEvent(*(undefined8 *)(lVar8 + 0x28));
+      LOCK();
+      *(int *)(lVar8 + 0x90) = *(int *)(lVar8 + 0x90) + 1;
+      UNLOCK();
+LAB_00006af0:
+      ExReleasePushLockExclusiveEx(param_3,0);
+      KeLeaveCriticalRegion();
+      pdVar14 = local_a8;
+      uVar7 = RtlConvertSidToUnicodeString(pdVar14,local_d0,1);
+      if (-1 < (int)uVar7) {
+        bVar5 = true;
+        pdVar14 = (dword *)&local_98;
+        uVar7 = RtlConvertSidToUnicodeString(pdVar14,param_6,1);
+        if (-1 < (int)uVar7) {
+          bVar6 = true;
+          pdVar14 = local_b8;
+          uVar7 = BfsOpenPolicyDirectory(local_b8,local_e0,local_a8,'\0',&local_c0);
+          if ((-1 < (int)uVar7) &&
+             (pdVar14 = local_b8,
+             uVar7 = BfsCreateStorage(local_b8,local_e0,local_c0,&local_98,&local_b0),
+             -1 < (int)uVar7)) {
+            RtlFreeUnicodeString(local_a8);
+            RtlFreeUnicodeString(&local_98);
+            KeEnterCriticalRegion();
+            ExAcquirePushLockExclusiveEx(param_3);
+            *(undefined8 *)(lVar8 + 0x30) = local_b0;
+            *(undefined4 *)(lVar8 + 0x38) = 0x10000000;
+            KeSetEvent(*(undefined8 *)(lVar8 + 0x28),0,0);
+            *local_88 = lVar8;
+            goto LAB_00006c44;
+          }
+        }
+      }
+      if (3 < DAT_00016000) {
+        local_e0 = CONCAT44(local_e0._4_4_,uVar7);
+        local_60 = &local_e0;
+        local_58 = 4;
+        _tlgWriteTransfer_EtwWriteTransfer(pdVar14,&DAT_00013c91);
+      }
+      goto LAB_00006c71;
+    }
+    LOCK();
+    *(int *)(lVar8 + 0x90) = *(int *)(lVar8 + 0x90) + 1;
+    UNLOCK();
+    ExReleasePushLockExclusiveEx(param_3);
+    KeLeaveCriticalRegion();
+    if (*(int *)(lVar8 + 0x38) == 0x10000001) {
+      uVar13 = *(undefined8 *)(lVar8 + 0x28);
+      KeWaitForSingleObject(uVar13,0,0,0);
+      if (*(int *)(lVar8 + 0x38) != 0x10000000) {
+        if (3 < DAT_00016000) {
+          local_e0 = CONCAT44(local_e0._4_4_,0xc0000001);
+          local_60 = &local_e0;
+          local_58 = 4;
+          _tlgWriteTransfer_EtwWriteTransfer(uVar13,&DAT_00013c91);
+          lVar10 = 0;
+          lVar9 = 0;
+        }
+        goto LAB_00006c76;
+      }
+    }
+    *param_7 = lVar8;
+  }
+  __security_check_cookie(local_50 ^ (ulonglong)auStackY_118);
+  return;
+}
+
